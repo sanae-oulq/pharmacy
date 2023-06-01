efdf20916f8a04a70325843552af4d7163a8a328
sanae <oulqaidsanae2003@gmail.com>
2023-06-01 23:11:56 +0100

    second commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3001'
+  origin: 'http://localhost:3000'
 };
 
 app.use(cors(corsOptions)); 
